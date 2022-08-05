800e94a0a553a4bc5afb4d8909beab387a527de0
nkere30 <ninokereselidze30@gmail.com>
2022-08-05 21:22:35 +0400

    Completed

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1, b = 1;
-        int formula = (9*(a^2) - 5*b + 2 + a - 7)*((a + b - 4*a*b)/2);
+        int formula = (9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
